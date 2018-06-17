fb32cfb12489f84fc4cd51ad4a43f94c34e4e9b2
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-06-17 17:36:03 -0500

    Add sections

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,4 @@ export default({children}) =>
 
     {children}
 
-    <svg viewBox="0 0 10 10" preserveAspectRatio="none">
-      <polygon strokeWidth="0" points="0 10, 10 0, 10 10"></polygon>
-      <polygon strokeWidth="0" fill="#148fce" points="0 10.01, 0 9, 10.01 0, 10.01 1"></polygon>
-    </svg>
-
   </header>
