aa2d13dd617baaa4e9e6da1a50f3c3d29106b529
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-06-17 13:12:18 -0500

    Tinkering around with organization

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/header.scss"
+
+export default({children}) =>
+  <header>
+    {children}
+    <svg viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon strokeWidth="0" points="0 10, 10 0, 10 10"></polygon>
+      <polygon strokeWidth="0" fill="#148fce" points="0 10.01, 0 8.5, 10.01 0, 10.01 1.5"></polygon>
+    </svg>
+  </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
 import React from "react"
-
-import logo from "../assets/banner_logo.svg"
+import Header from "../components/header"
 
 import "../styles/layout.scss"
 
+import logo from "../assets/banner_logo.svg"
+
 export default () => 
 <div className="layout">
-
-  <header className="banner">
+  <Header>
     <img src={logo} className="logo"/>
-
-    <svg viewBox="0 0 10 10" preserveAspectRatio="none">
-      <polygon strokeWidth="0" points="0 10, 10 0, 10 10"></polygon>
-      <polygon strokeWidth="0" fill="#148fce" points="0 10.01, 0 8.5, 10.01 0, 10.01 1.5"></polygon>
-    </svg>
-  </header>
-
+  </Header>
 </div>
