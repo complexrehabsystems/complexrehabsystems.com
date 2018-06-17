6c88738e8730b21e3d7c43f2b46d68f25f40ac5f
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-06-17 13:47:17 -0500

    Add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 import "../styles/layout.scss"
 
@@ -7,8 +8,16 @@ import logo from "../assets/banner_logo.svg"
 
 export default () => 
 <div className="layout">
-  <Header>
+
+  <Header className="site-header">
     <img src={logo} className="logo"/>
   </Header>
+
+  <div className="content">content</div>
+
+  <Footer className="site-footer">
+    <span>contact info</span>
+    <span>etc</span>
+  </Footer>
 
 </div>
