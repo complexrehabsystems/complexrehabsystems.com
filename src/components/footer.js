a0510811359e7cab71942f84285191e971986e92
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-06-17 17:36:58 -0500

    Add sections

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,11 +4,6 @@ import "../styles/footer.scss"
 export default({children}) =>
   <footer>
 
-    <svg viewBox="0 0 10 10" preserveAspectRatio="none">
-      <polygon strokeWidth="0" points="0 0, 0 10, 10 0"></polygon>
-      <polygon strokeWidth="0" fill="#ed7d31" points="0 10.01, 0 8.5, 10.01 0, 10.01 1.5"></polygon>
-    </svg>
-
     <div className="footer-items">
       {children}
     </div>
