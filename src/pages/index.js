72f486b9e711f200f8ad4acbda823a0766f7e19e
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-07-02 15:28:11 -0500

    Update index.js
    Just a dummy change

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,7 @@ export default () =>
       <section id="about">
         <h1>About CRS</h1>
         <div className="section-content">
-          <h2>(dummy text)</h2>
+          <h2>(this is really dummy text)</h2>
         </div>
       </section>
 
