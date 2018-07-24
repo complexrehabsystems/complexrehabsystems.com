749a070f919510c587e93f21dc7768c210f2c44c
HimaBharathi <hadusumilli@wisc.edu>
2018-07-24 17:10:16 -0500

    Crs 650  rework site navigation icons (#5)
    * icons added
    
    * make buttons responsive, add text
    
    * wrapped up working on icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,22 @@ import tom from "../assets/tom.jpg"
 import avatar from "../assets/avatar.png"
 import hima from "../assets/himadusumilli.jpg"
 
+import activeInfo from "../assets/nav-icons/crs-active-info.png";
+import activeTeam from "../assets/nav-icons/crs-active-team.png";
+import activeContact from "../assets/nav-icons/crs-active-contact.png";
+import hoverInfo from "../assets/nav-icons/crs-hover-info.png";
+import hoverTeam from "../assets/nav-icons/crs-hover-team.png";
+import hoverContact from "../assets/nav-icons/crs-hover-contact.png";
+import staticInfo from "../assets/nav-icons/crs-static-info.png";
+import staticTeam from "../assets/nav-icons/crs-static-team.png";
+import staticContact from "../assets/nav-icons/crs-static-contact.png";
+import leftPadding from "../assets/nav-icons/crs-left-padding.png";
+import rightPadding from "../assets/nav-icons/crs-right-padding.png";
+import bgRepeat from "../assets/nav-icons/crs-bg-repeat.png";
+
+
+
+
 let images = {
   "Douglas Munsey": doug,
   "Thomas Whelan": tom,
@@ -113,30 +129,48 @@ export default ( {data}) => {
 
     <div className="content">
 
-      <section id="nav" className="ctas">
+      <div id="nav" className="ctas">
+
+        <a>
+          <div className="cta">
+            <img src={leftPadding} className="left-padding"/>
+          </div>
+        </a>
 
         <a href="#about">
           <div className="cta">
-            <InfoIcon className="cta-icon"/>
-            <h4 className="cta-info">About CRS</h4>
+            <img src={staticInfo} className="cta-icon static"/>
+            <img src={hoverInfo} className="cta-icon hover"/>
+            <img src={activeInfo} className="cta-icon active"/>
+            <h4>WHAT WE DO</h4>
           </div>
         </a>
 
         <a href="#team">
           <div className="cta">
-            <PeopleIcon className="cta-icon"/>
-            <h4 className="cta-info">Meet the Team</h4>
+            <img src={staticTeam} className="cta-icon static"/>
+            <img src={hoverTeam} className="cta-icon hover"/>
+            <img src={activeTeam} className="cta-icon active"/>
+            <h4>WHO WE ARE</h4>
           </div>
         </a>
 
-        <a href="#signup">
+        <a href="#contact">
           <div className="cta">
-            <MailIcon className="cta-icon"/>
-            <h4 className="cta-info">Signup for E-Mail Updates</h4>
+            <img src={staticContact} className="cta-icon static"/>
+            <img src={hoverContact} className="cta-icon hover"/>
+            <img src={activeContact} className="cta-icon active"/>
+            <h4>GET IN TOUCH</h4>
           </div>
         </a>
 
-      </section>
+        <a>
+          <div className="cta">
+            <img src={rightPadding} className="right-padding" />
+          </div>
+        </a>
+
+      </div>
 
       <div className="sections">
 
