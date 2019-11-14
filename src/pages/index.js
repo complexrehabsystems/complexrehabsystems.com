398afecb033bb821f0add08e66ee91b6a295d3d5
Daniel Packard <daniel.packard@complexrehabsystems.com>
2019-11-14 09:32:59 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -255,7 +255,7 @@ export default ({ data }) => {
                         <h2>{getInTouchInfo.tagline}</h2>
                         <h2 className="signup-success">{getInTouchInfo.successMessage}</h2>
                         <form id="contact" name="contact" method="POST" action="/?success" data-netlify="true" netlify>
-                            <p class="hidden">
+                            <p style="display:none">
                               <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
                             </p>
                             <input type="hidden" name="form-name" value="contact" />
