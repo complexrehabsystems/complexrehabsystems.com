5a0782e2112724ee27d38167364a437a75fe6f97
Daniel Packard <pack3754@gmail.com>
2020-07-10 17:58:29 -0500

    Add FAQ

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,37 +1,20 @@
-import React from "react"
-import "../styles/layout.scss"
+import React from 'react';
+import '../styles/layout.scss';
+import logo from '../assets/logo_dark.png';
 
 export default ({ data }) => {
-  return <div className="layout support" >
-    <header>
-      <h1> Complex Rehab Systems (CRS) Support &nbsp;
-      <div className="small-link"><a href="/#top">back to CRS site</a></div>
-      </h1>
-    </header>
-
-    <div className="content">
-
-      <div>
-        <h1 id="user-manuals">User Manuals</h1>
-        <ul>
-          <li>
-            <a href="/pdfs/QuickStartGuide_20200626.pdf">Quick Start Guide</a>
-          </li>
-          <li>
-            <a href="/pdfs/DetailedUsageGuide_20200626.pdf">Detailed Usage Guide</a>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <h1 id="video-tutorials">Video Tutorials</h1>
-        <p><a href="https://vimeo.com/showcase/crs-tutorials" target="blank">View Video Tutorials on Vimeo</a></p>
-        <div className="vimeo-showcase-embed-wrapper"><iframe src='https://vimeo.com/showcase/7006209/embed' allowfullscreen frameBorder='0' className="vimeo-showcase-embed-iframe"></iframe></div>
+  return (
+    <div className='layout support'>
+      <div className='content'>
+      <div className='logo-mission'>
+        <img src={logo} className='faq-logo' />
       </div>
 
         <div id='faq'>
-          <h1>FAQs</h1>
-          <h2>Frequently Asked Questions</h2>
+          <div className="faq-headers">
+            <h1>FAQs</h1>
+            <h2>Frequently Asked Questions</h2>
+          </div>
 
           <dl>
             <dl className='faq-question'>1. What is CRS?</dl>
@@ -90,31 +73,7 @@ export default ({ data }) => {
             </dt>
           </dl>
         </div>
-
-      <div>
-        <h1 id="contact-us">Contact Us</h1>
-        <p>
-          For additional help, our Tech Support Team can be reached
-          during regular business hours by email or telephone:
-        </p>
-        <ul>
-          <li>
-            <b>Email:</b> <b>
-              <a href="mailto:crs.support@kimobility.com">crs.support@kimobility.com</a>
-            </b>
-          </li>
-          <li>
-            <b>Phone:</b> (715) 997-9979
-          </li>
-        </ul>
-        <p> Our technical support business hours are: <b>Monday – Friday, 8:00am – 5:00pm CST</b></p>
-        <h1>Feedback</h1>
-        <p>We want to hear from you!</p>
-        <p>
-          If you have any feedback to share about the app, including ideas for new features you can do so by emailing <a href="mailto:crs.support@kimobility.com">crs.support@kimobility.com</a>, or clicking
-          the icon in the header of the CRS application.
-        </p>
       </div>
     </div>
-  </div>
-}
+  );
+};
